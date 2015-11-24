22e2bc1f2b80d33257c4fc3b5672067403bea600
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-11-24 16:03:44 -0500

    Project skeleton

---
+++ src/LoadRunnerDocumentationAddin/Pads/DocumentationPad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ICSharpCode.SharpDevelop.Gui;
+using MyLoadTest.LoadRunnerDocumentation.AddIn.Controls;
+
+namespace MyLoadTest.LoadRunnerDocumentation.AddIn.Pads
+{
+    public sealed class DocumentationPad : AbstractPadContent
+    {
+        #region Constants and Fields
+
+        private readonly DocumentationControl _innerControl;
+
+        #endregion
+
+        #region Constructors
+
+        public DocumentationPad()
+        {
+            _innerControl = new DocumentationControl();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public override object Control
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _innerControl;
+            }
+        }
+
+        #endregion
+    }
+}
